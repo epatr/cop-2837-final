50b6250563594952249e7fbe8d4194adfca4aa39
Eric Patrick <erstpa@gmail.com>
2018-08-09 20:28:30 -0400

    Finished creating main sub procedures and functions

--- TicTacToe/TicTacToe/Main Form.vb
+++ TicTacToe/TicTacToe/Main Form.vb
@@ -5,17 +5,46 @@ Option Explicit On
 Option Infer Off
 
 Public Class frmMain
-    Private Sub btnExit_Click(sender As Object, e As EventArgs)
-        Me.Close()
-    End Sub
 
     Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
         Me.Close()
     End Sub
 
+    Private Sub NewGameToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewGameToolStripMenuItem.Click
+        StartNewGame()
+    End Sub
+
+    Private Sub StartNewGame()
+        btn1.Text = String.Empty
+    End Sub
+
+    Private Function CheckForWin() As Boolean
+        ' Check bottom row
+        If btn1.Text = "O" Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+
     Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
         If btn1.Text = String.Empty Then
             btn1.Text = "O"
         End If
+
+        If CheckForWin() Then
+            Dim result As Integer = MessageBox.Show("You've won! Play again?", "Victory!", MessageBoxButtons.YesNo)
+            If result = DialogResult.No Then
+                Me.Close()
+            ElseIf result = DialogResult.Yes Then
+                StartNewGame()
+            End If
+        End If
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        MessageBox.Show("Program by Eric Patrick." & Environment.NewLine & "Source code available at https://github.com/epatr/cop-2837-final", "About", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
     End Sub
 End Class
